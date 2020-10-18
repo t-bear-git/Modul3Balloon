f3b1ed43bcddeac4665799e74c40986abe892582
t-bear-git <60154712+t-bear-git@users.noreply.github.com>
2020-10-18 18:58:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Contructor Method for sound-effect
 class sound {
   constructor(src) {
     this.sound = document.createElement("audio");
@@ -13,15 +14,21 @@ class sound {
     };
   }
 }
+
+// Eventlistener, declaring variables to add correct class after event.
 document.addEventListener("mouseover", (event) => {
   const isBalloon = event.target.classList.contains("balloon");
   const isPopped = event.target.classList.contains("popped");
+  // if statement to evaluate what class should show.
   if (isBalloon && !isPopped) {
     event.target.classList.add("popped");
+    // running sound effect when event takes place.
     pop = new sound("blop.mp3");
     pop.play();
+    // Variables for counting elements with specified classes.
     const poppedCount = document.querySelectorAll(".balloon.popped").length;
     const balloonCount = document.querySelectorAll(".balloon").length;
+    // if statement to show Congrats Message when all balloons is popped.
     if (poppedCount === balloonCount) {
       console.log("All popped!");
       const balloons = document.querySelector(".balloons");
